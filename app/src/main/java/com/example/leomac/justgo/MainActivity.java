8dde9d9bafaa7703d572a31a2f789eec93c11b43
leo <leosonus@gmail.com>
2017-09-23 03:43:47 +0900

    second fix

--- app/src/main/java/com/example/leomac/justgo/MainActivity.java
+++ app/src/main/java/com/example/leomac/justgo/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.leomac.justgo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mButtonPress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("Just","Hello");
+
+        mButtonPress = (Button)findViewById(R.id.bt_press_next);
+        mButtonPress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
